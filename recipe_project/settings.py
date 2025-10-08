c195eca8a252ca9f7964371af511465f88ed6923
Edwin Ouko <edwinouko4@gmail.com>
2025-10-07 22:00:40 -0700

    progress checkpoint

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -1,15 +1,37 @@
-
+from pathlib import Path
 
 MAX_COOK_TIME = 240.0  # Maximum cooking time in minutes
 
+ROOT_URLCONF = 'recipe_project.urls'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+# DATABASES = { # TODO:requires installation of postgres
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'recipe_db',
+#         'USER': 'user',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+DEBUG = True
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'recipe_db',
-        'USER': 'user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
